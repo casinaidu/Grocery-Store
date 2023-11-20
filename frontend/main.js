25152410924831c6cf30ed099e5c4c68542194e6
shabarish1403 <shabarish.14b@gmail.com>
2023-11-20 17:11:56 +0530

    abc

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,4 @@
-import router from "./js/router.js"
+// import router from "./js/router.js"
 
 import component1 from "./components/component1.js"
 import component2 from "./components/component2.js"
@@ -8,7 +8,7 @@ Vue.component('component2',component2)
 
 var app = new Vue({
   el: '#app',
-  router,
+  // router,
   data: {
     message: 'Hello Shab!',
     name: 'shabarish'
