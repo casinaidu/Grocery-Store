4d05fa086eb32f1648efc4473bca620f31d927ae
shabarish1403 <shabarish.14b@gmail.com>
2023-12-09 13:40:03 +0530

    changes

--- frontend/main.js
+++ frontend/main.js
@@ -1,20 +1,23 @@
-import store from "./js/store.js"
+// import store from "./js/store.js"
 
 import component1 from "./components/component1.js"
-import component2 from "./components/component2.js"
+// import component2 from "./components/component2.js"
 
 Vue.component('component1',component1)
-Vue.component('component2',component2)
+// Vue.component('component2',component2)
 
 import router from "./js/router.js"
 
 var app = new Vue({
   el: '#app',
+  data: {
+    message: 'hello'
+  },
   router,
-  store,
-  computed: {
-    message() {
-      return this.$store.state.message
-    }
-  }
+  // store,
+  // computed: {
+  //   // message() {
+  //   //   return this.$store.state.message
+  //   // }
+  // }
 })
